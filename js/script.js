2e772c11cdd941147515fcc94d5d7d91970e74e7
Andrii <git@testemail.com>
2020-08-05 11:10:35 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert('Hi, friend!')
+
+// const result = +prompt('how ola are you?', '18');
+
+// const answers = [];
+// answers[0] = prompt('Name?', '');
+// answers[1] = prompt('Age?', '');
+// answers[2] = prompt('Man?', '');
+
+const category = 'toys';
+
+
+
+console.log(`http://toys.com/${category}/4`);
