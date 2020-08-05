53af38e39ba3d72765e3401c93a6996250bc5136
Andrii <git@testemail.com>
2020-08-05 12:16:01 +0300

    make dz1

---
+++ dz1.js
@@ -0,0 +1,46 @@
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+const numberOfFilms = +prompt('How many films did you see?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    provat: false
+};
+
+let filmName = prompt("Last film, what you've seen?", '');
+let filmRate = +prompt(`How you can rate film ${filmName}? `, '');
+
+personalMovieDB.movies[filmName]= filmRate;
+
+filmName = prompt("Last film, what you've seen?", '');
+filmRate = +prompt(`How you can rate film ${filmName}? `, '');
+
+personalMovieDB.movies[filmName]= filmRate;
+
+console.log(personalMovieDB);
